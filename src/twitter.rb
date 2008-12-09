1b9d749b76662ea943d3dc0531a31be084b0f450
Nuno Job <dscape@dscape-2.local>
2008-12-08 21:35:16 -0500

    Added link support to the app

--- src/twitter.rb
+++ src/twitter.rb
@@ -61,9 +61,21 @@ Shoes.app :title => Rudolph::SYS_USR, :width => Rudolph::APP_WIDTH,
         else
           background "#fff", :curve => Rudolph::UPDT_CURVE
         end
-        para strong(user, :stroke => @theme[:link]), ' ', message, :margin => Rudolph::UPDT_PMARGIN
+        eval "para strong(user, :stroke => @theme[:link]), \" \", #{process_links message}, :margin => Rudolph::UPDT_PMARGIN"
       end
     end
+  end
+  
+  def process_links message
+    message.split.map do |token|
+      if token =~ /((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+        %Q(link("#{token }", :click => "#{token}", :stroke => @theme[:link]), ' ')
+      elsif token =~ /@[a-z0-9_]+/i
+        %Q(\"@\", link("#{token.gsub('@','')}", :click => "http://www.twitter.com/#{token.gsub('@','')}", :stroke => @theme[:link]), ' ')
+      else
+        "\"#{token} \""
+      end
+    end.join(', ')
   end
 
   def if_valid(request)    
